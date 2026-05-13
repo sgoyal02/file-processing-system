509e407a7b5b521eb78f27ba975d5624bcf3e6b3
sgoyal02 <122796484+sgoyal02@users.noreply.github.com>
2026-05-13 20:04:45 +0530

    update: routing and css styling files

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,5 +1,5 @@
 import { useAuth } from "../contexts/AuthContext";
-import type { LoginCreds, LoginResult, Project} from "./types";
+import type { LoginCreds, LoginResult, Project, SavedFile} from "./types";
 
 const BASE_URL = 'http://localhost:4000';
 
@@ -50,7 +50,7 @@ const fetchProjects=async(): Promise<Project[]>=> {
   }
 
  const createProject=async(data: { name: string; description: string }): Promise<Project> =>{
-    const res = await fetch(`${BASE_URL}/projects`, {
+    const res = await fetch(`${BASE_URL}/projects`, {  //or ?_embed=files&_embed=jobs  ??
       method: 'POST',
       headers: authHeaders(),
       body: JSON.stringify({ ...data, createdAt: new Date().toISOString(), 
@@ -73,5 +73,65 @@ const fetchProjects=async(): Promise<Project[]>=> {
     if (!res.ok) throw new Error('Delete fail');
   }
 
-    return {handleLogin, isTokenMiss, createProject, fetchProjects, fetchProjectById, removeProject}
+  const fetchFiles=async(id:string): Promise<SavedFile[]>=> {
+    const res = await fetch(`${BASE_URL}/files?projectId=${id}`, { headers: authHeaders() });
+    return handleResponse<SavedFile[]>(res);
+  }
+
+  const uploadFile= async(file:File, id:string | number,
+    onProgress:(percent:number)=>void):Promise<SavedFile>=>{
+      await new Promise((resolve, reject)=>{
+        let pr=0;
+        const gap=setInterval(()=>{
+          pr=pr+Math.floor(Math.random()*30)+10;
+          if(pr>=100){
+            pr=100;
+            onProgress(100);
+            clearInterval(gap);
+              resolve(true);
+          } else{
+            onProgress(pr);
+          }
+        }, 400);
+      });
+      const fileData={
+        id: crypto.randomUUID(),  //or omit<savedfiel, 'id'>
+        name:file.name,
+                projectId:id,size: file.size, uploadedAt: new Date().toISOString()
+              }
+      return addFileToProject(fileData);
+  }
+  const addFileToProject = async(fileData:SavedFile): Promise<SavedFile> =>{
+    const res = await fetch(`${BASE_URL}/files`, {
+      method: 'POST',
+      headers: authHeaders(),
+      body: JSON.stringify(fileData),
+    });
+    return handleResponse<SavedFile>(res);
+  }
+
+  const updateProjectDataCount = async (id: string, 
+    newCount: { filesCount?: number; jobsCount?: number }
+) => {
+  const res = await fetch(`${BASE_URL}/projects/${id}`, {
+    method: 'PATCH',
+    headers: authHeaders(),
+    body: JSON.stringify(newCount),
+  });
+  return res.ok;
+};
+
+    const delFile=async(id: string):Promise<void>=> {
+    const res = await fetch(`${BASE_URL}/files/${id}`, {
+      method: 'DELETE',
+      headers: authHeaders(),
+    });
+    console.log("res api: ", res);
+    if (!res.ok) throw new Error('Delete file fail');
+  }
+
+    return {handleLogin, isTokenMiss, createProject, fetchProjects, 
+      fetchProjectById, removeProject, fetchFiles,
+      uploadFile, delFile, updateProjectDataCount
+    }
 }
