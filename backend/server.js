8b93d0b3c4b0dfe6d2d7c6e5196b25bbc7c4944f
sgoyal02 <122796484+sgoyal02@users.noreply.github.com>
2026-05-15 13:29:06 +0530

    chore: setup json-server backend for deployment

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const jsonServer= require('json-server');
+const app= jsonServer.create();
+const api= jsonServer.router("db.json");
+const middleware= jsonServer.defaults();
+
+const PORT= process.env.port||4000;
+app.use(middleware);
+app.use(api);
+app.listen(PORT,"0.0.0.0", ()=>{
+    console.log("json server running on: ", PORT);
+})
