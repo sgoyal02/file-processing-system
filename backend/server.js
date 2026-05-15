a66935f2ac9ddb1d3f525edbfc488760f4158755
sgoyal02 <122796484+sgoyal02@users.noreply.github.com>
2026-05-15 13:52:21 +0530

    fix:server file for render

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
-const jsonServer= require('json-server');
-const app= jsonServer.create();
-const api= jsonServer.router("db.json");
-const middleware= jsonServer.defaults();
+const jsonServer = require("json-server");
 
-const PORT= process.env.PORT||4000;
-app.use(middleware);
-app.use(api);
-app.listen(PORT,"0.0.0.0", ()=>{
-    console.log("json server running on: ", PORT);
-})
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 4000;
+app.use(middlewares);
+app.use(router);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("json Server running on port " + PORT);
+});
